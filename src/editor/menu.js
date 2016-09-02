74c9f8d1a9bbae5e80e18428a16cc1b48029b5b8
Jacob Wright <jacwright@gmail.com>
2016-09-02 00:41:46 -0600

    Update temp menu

--- src/editor/menu.js
+++ src/editor/menu.js
@@ -5,21 +5,36 @@ menu.id = 'editor-menu';
 var currentEditor;
 var mouseDown = false;
 var lastSelectionEvent;
-menu.innerHTML = '<button onclick="dabble.activeEditor.toggleBlockType(\'header2\')">H1</button><button onclick="dabble.activeEditor.toggleBlockType(\'header3\')">H2</button><div class="editor-button-separator"></div><button onclick="dabble.activeEditor.toggleMarkup(\'bold\')">B</button><button style="font-style:italic" onclick="dabble.activeEditor.toggleMarkup(\'italic\')">I</button>';
+menu.innerHTML = `
+<button onclick="dabble.activeEditor.toggleBlockType(\'h2\');dabble.activeEditor.element.focus()">H1</button>
+<button onclick="dabble.activeEditor.toggleBlockType(\'h3\');dabble.activeEditor.element.focus()">H2</button>
+<div class="editor-button-separator"></div>
+<button onclick="dabble.activeEditor.toggleMarkup(\'strong\');dabble.activeEditor.element.focus()">B</button>
+<button style="font-style:italic" onclick="dabble.activeEditor.toggleMarkup(\'em\');dabble.activeEditor.element.focus()">I</button>
+`;
 
 
 
 
 menu.reposition = function() {
+  var container = menu.parentNode;
+  if (!container) return;
   var rect = currentEditor.selection.rect;
-  var offsetParent = currentEditor.element.offsetParent || document.documentElement;
-  var containerRect = offsetParent.getBoundingClientRect();
-  menu.style.left = Math.floor(rect.left - containerRect.left - (menu.offsetWidth - rect.width)/2) + 'px';
-  menu.style.top = (rect.top - containerRect.top - menu.offsetHeight - 6) + 'px';
+  var containerRect = container.getBoundingClientRect();
+  menu.style.left = Math.floor(rect.left - containerRect.left + container.scrollLeft - (menu.offsetWidth - rect.width)/2) + 'px';
+  menu.style.top = (rect.top - containerRect.top + container.scrollTop - menu.offsetHeight - 6) + 'px';
 };
 
 menu.show = function() {
-  currentEditor.element.parentNode.insertBefore(menu, currentEditor.element.nextSibling);
+  var scroller = currentEditor.element;
+  while (scroller) {
+    var overflow = getComputedStyle(scroller).overflow;
+    if (overflow === 'scroll' || overflow === 'auto' || scroller.parentNode.nodeType !== Node.ELEMENT_NODE) {
+      break;
+    }
+    scroller = scroller.parentNode;
+  }
+  scroller.appendChild(menu);
   requestAnimationFrame(function() {
     menu.classList.add('active');
   });
@@ -31,7 +46,8 @@ menu.hide = function() {
 };
 
 
-document.addEventListener('editorselectionchanged', updateSelection);
+document.addEventListener('editorselectionchange', updateSelection);
+document.addEventListener('editorchange', menu.reposition);
 document.addEventListener('mousedown', function() {
   mouseDown = true;
 });
