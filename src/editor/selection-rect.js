196c549aca0628eb97f7ec9a162b25d6d162c874
Jacob Wright <jacwright@gmail.com>
2016-09-29 09:18:17 -0700

    But fixes

--- src/editor/selection-rect.js
+++ src/editor/selection-rect.js
@@ -10,14 +10,18 @@ exports.get = function() {
     var rect = rects[rects.length - 1];
     if (!rect && range.startContainer.nodeType === Node.ELEMENT_NODE) {
       var child = range.startContainer.childNodes[range.startOffset];
-      rect = child.getBoundingClientRect();
+      if (child.getBoundingClientRect) {
+        rect = child.getBoundingClientRect();
+      } else {
+        return;
+      }
     }
-    if (!rect) {
-      var shadowCaret = document.createTextNode('|');
-      range.insertNode(shadowCaret);
-      rect = range.getBoundingClientRect();
-      shadowCaret.parentNode.removeChild(shadowCaret);
-    }
+    // if (!rect) {
+    //   var shadowCaret = document.createTextNode('|');
+    //   range.insertNode(shadowCaret);
+    //   rect = range.getBoundingClientRect();
+    //   shadowCaret.parentNode.removeChild(shadowCaret);
+    // }
     return new Rect(rect);
   } else {
     return new Rect(selection.getRangeAt(0).getBoundingClientRect());
