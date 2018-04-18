744d474bf945713aa42e4a645e6620b888f6be64
Jacob Wright <jacwright@gmail.com>
2018-04-18 08:02:45 -0600

    Bumped version

--- dist/index.js
+++ dist/index.js
@@ -4085,6 +4085,9 @@ function isTextEntry(change) {
 
 /**
  * Replaces regular quotes with smart quotes as they are typed. Does not affect pasted content.
+ * Uses the text-changing event to prevent the original change and replace it with the new one. This makes the smart-
+ * quotes act more seemlessly and includes them as part of regular text undo/redo instead of breaking it like the smart-
+ * entry conversions do.
  */
 function smartQuotes(view) {
   var editor = view.editor;
--- src/modules/smart-quotes.js
+++ src/modules/smart-quotes.js
@@ -1,6 +1,9 @@
 
 /**
  * Replaces regular quotes with smart quotes as they are typed. Does not affect pasted content.
+ * Uses the text-changing event to prevent the original change and replace it with the new one. This makes the smart-
+ * quotes act more seemlessly and includes them as part of regular text undo/redo instead of breaking it like the smart-
+ * entry conversions do.
  */
 export default function smartQuotes(view) {
   const editor = view.editor;
