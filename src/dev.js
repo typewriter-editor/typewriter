dac57f2655b45e430d7886e89ca35037105c6654
Jacob Wright <jacwright@gmail.com>
2018-04-27 13:12:40 -0600

    Add styling and instructions to dev

--- src/dev.js
+++ src/dev.js
@@ -15,14 +15,8 @@ window.editor = editor;
 window.view = view;
 
 // ***** Search feature
-const style = document.createElement('style');
-style.textContent = 'span.search { background: yellow }';
-document.head.appendChild(style);
-
-const searchInput = document.createElement('input');
+const searchInput = document.querySelector('.search-input');
 let searchString = '';
-searchInput.type = 'search';
-document.body.appendChild(searchInput);
 searchInput.addEventListener('input', () => {
   searchString = searchInput.value.toLowerCase().trim();
   view.update();
@@ -65,22 +59,20 @@ view.on('update', () => {
 });
 // ***** Search feature
 
-view.paper.embeds.add(cursor);
-let pos = 10;
-let changes = editor.delta();
+// view.paper.embeds.add(cursor);
+// let pos = 10;
+// let changes = editor.delta();
 
-view.on('decorate', (editor, event) => {
-  if (event && event.change && event.source === 'user') {
-    changes = changes.compose(event.change);
-  }
-  editor.insertEmbed(changes.transform(pos), 'cursor', { name: 'Jacob', color: 'blue' });
-});
+// view.on('decorate', (editor, event) => {
+//   if (event && event.change && event.source === 'user') {
+//     changes = changes.compose(event.change);
+//   }
+//   editor.insertEmbed(changes.transform(pos), 'cursor', { name: 'Jacob', color: 'blue' });
+// });
 
 
 
-// editor.setText(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis sagittis libero. Etiam egestas rhoncus risus, sed accumsan nisi laoreet a. Praesent pulvinar porttitor lorem, vel tempor est vulputate nec. Duis magna justo, ultrices at ullamcorper a, sagittis quis mi. Duis id libero non augue faucibus faucibus sed nec sapien. Vivamus pulvinar justo nec metus dapibus, quis tincidunt justo fermentum. Aliquam erat volutpat. Nam hendrerit libero ut nunc rutrum pellentesque. Nulla erat eros, molestie ac nibh non, consectetur luctus lorem. Mauris vel egestas nisi.
-// Mauris sed mi cursus urna pretium posuere sit amet id lorem. Maecenas tristique commodo diam at elementum. Maecenas dapibus risus at mauris consequat, ac semper justo commodo. Sed tempor mattis nisi, in accumsan felis gravida non. In dignissim pellentesque ornare. Mauris lorem sem, consectetur eu ornare at, laoreet sed dui. Nam gravida justo tempus ligula pharetra, sit amet vestibulum lorem sagittis. In mauris purus, cursus vitae tempus at, tincidunt et arcu. Etiam sed libero ac mi fermentum hendrerit. Cras vel cursus urna, sed pretium nisl. Mauris sodales tempor ex nec iaculis. Nulla ac erat ac nunc malesuada viverra. Pellentesque nec ipsum in arcu consectetur elementum a ut metus. Integer sit amet eleifend nulla. Morbi ac felis malesuada, dapibus libero eget, posuere neque. Cras porta ut metus sed vulputate.`);
-editor.setText(`Lorem ipsum dolor sit amet.
-Mauris sed mi cursus urna pretium posuere sit amet id lorem.`);
+editor.setText(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis sagittis libero. Etiam egestas rhoncus risus, sed accumsan nisi laoreet a. Praesent pulvinar porttitor lorem, vel tempor est vulputate nec. Duis magna justo, ultrices at ullamcorper a, sagittis quis mi. Duis id libero non augue faucibus faucibus sed nec sapien. Vivamus pulvinar justo nec metus dapibus, quis tincidunt justo fermentum. Aliquam erat volutpat. Nam hendrerit libero ut nunc rutrum pellentesque. Nulla erat eros, molestie ac nibh non, consectetur luctus lorem. Mauris vel egestas nisi.
+Mauris sed mi cursus urna pretium posuere sit amet id lorem. Maecenas tristique commodo diam at elementum. Maecenas dapibus risus at mauris consequat, ac semper justo commodo. Sed tempor mattis nisi, in accumsan felis gravida non. In dignissim pellentesque ornare. Mauris lorem sem, consectetur eu ornare at, laoreet sed dui. Nam gravida justo tempus ligula pharetra, sit amet vestibulum lorem sagittis. In mauris purus, cursus vitae tempus at, tincidunt et arcu. Etiam sed libero ac mi fermentum hendrerit. Cras vel cursus urna, sed pretium nisl. Mauris sodales tempor ex nec iaculis. Nulla ac erat ac nunc malesuada viverra. Pellentesque nec ipsum in arcu consectetur elementum a ut metus. Integer sit amet eleifend nulla. Morbi ac felis malesuada, dapibus libero eget, posuere neque. Cras porta ut metus sed vulputate.`);
 
 view.mount(document.body);
